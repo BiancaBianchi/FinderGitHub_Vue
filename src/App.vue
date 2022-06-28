bb3bbcc426dad526a7d651ce35129df74e2063ad
Bianca <biabianchi7398@gmail.com>
2022-06-28 04:13:35 -0300

    1-Finder git hub

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,79 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Navbar />
+    <div class="container">
+      <div class="search card card-body">
+        <h1>Pesquisar Usuários do GitHub</h1>
+        <p class="lead">
+          Digite um nome para encontrar usuários e repositórios
+        </p>
+        <input
+          @keyup="getUser"
+          class="form-control"
+          placeholder="Digite o nome de um usuário..."
+          required
+        />
+      </div>
+
+      <div
+        class="row mt-3"
+        v-if="user.length !== 0"
+      >
+        <div class="col-md-4">
+          <Profile :user="user" />
+        </div>
+        <div class="col-md-8">
+          <Repos
+            v-for="repo in repos"
+            :repo="repo"
+            :key="repo"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios";
+import Navbar from "./components/Navbar.vue";
+import Profile from "./components/Profile.vue";
+import Repos from "./components/Repos.vue";
 export default {
-  name: 'App',
+  name: "app",
+  data() {
+    return {
+      github: {
+        url: "https://api.github.com/users",
+        client_id: "efe6d142e68b2dfa82f6",
+        client_secret: "bd7e2fcd4bddfe163dae4a5df17c0830556bb3f2",
+        count: 7,
+        sort: "created: asc"
+      },
+      user: [],
+      repos: []
+    };
+  },
   components: {
-    HelloWorld
+    Navbar,
+    Profile,
+    Repos
+  },
+  methods: {
+    getUser(e) {
+      const user = e.target.value;
+      const { url, client_id, client_secret, count, sort } = this.github;
+      axios
+        .get(
+          `${url}/${user}?client_id=${client_id}&client_secret=${client_secret}`
+        )
+        .then(({ data }) => (this.user = data));
+      axios
+        .get(
+          `${url}/${user}/repos?per_page=${count}&sort=${sort}&client_id=${client_id}&client_secret=${client_secret}`
+        )
+        .then(({ data }) => (this.repos = data));
+    }
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'bootstrap'
+import 'jquery'
 
 createApp(App).mount('#app')
